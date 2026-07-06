ac48cdbe9561a3820f2fcd30d5528f9123cb7a7e
Erkan HAZIR <41erkan2002@gmail.com>
2026-07-06 09:41:22 +0300

    fix: PackageModel Name

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -195,4 +195,4 @@ class PackageConfigs(Configs):
 class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["component"] = "component"
-    name: Literal["DemoPackage"] = "DemoPackage"
+    name: Literal["RgbNormalization"] = "RgbNormalization"
