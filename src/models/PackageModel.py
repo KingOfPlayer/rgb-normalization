802c898a94d8ecc7e5d272f01cddfa01a410b2c5
Erkan HAZIR <41erkan2002@gmail.com>
2026-07-09 17:42:07 +0300

    feat: Dynamic test

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,10 +1,114 @@
 
-from pydantic import Field, validator
-from typing import List, Optional, Union, Literal
+from pydantic import Field, create_model
+from pydantic._internal._model_construction import ModelMetaclass
+from typing import List, Optional, Union, Literal, Dict, Any
 from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
 
-# Image Class
+# region Dynamic
+# Base definations 
+class BaseInputImage(Input):
+    name: Literal["baseInputImage"] = "baseInputImage"
+    value: Union[List[Image], Image]
+    type: str = "object"
+    class Config: title = "Image"
 
+class BaseOutputImage(Output):
+    name: Literal["baseOutputImage"] = "baseOutputImage"
+    value: Union[List[Image], Image]
+    type: str = "object"
+    class Config: title = "Image"
+
+def _make_image_model(base_cls, field_name: str):
+    """
+    Create a subclass of base_cls (BaseInputImage or BaseOutputImage)
+    whose `name` Literal is pinned to field_name, matching the key
+    it will be assigned to on the parent model.
+    """
+    return create_model(
+        f"{base_cls.__name__}_{field_name}",
+        __base__=base_cls,
+        name=(Literal[field_name], field_name),
+    )
+
+# Config
+class InputOutputCount(Config):
+    """
+        "Input Output Count"
+    """
+    name: Literal["InputOutputCount"] = "InputOutputCount"
+    value: int = Field(default=1, ge=1, le=5)
+    type: Literal["number"] = "number"
+    field: Literal["textInput"] = "textInput"
+
+    class Config:
+        title = "Input Output Count"
+
+class DynamicFieldsMetaclass(ModelMetaclass):
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        annotations = namespace.get("__annotations__", {})
+
+        # Pull the default count instead of hardcoding range(1, 6)
+        default_count = InputOutputCount.model_fields["value"].default  # pydantic v2
+        # default_count = InputOutputCount.__fields__["value"].default  # pydantic v1
+
+        if "Inputs" in name or name == "ExecutorDynamicInputs":
+            base_input_cls = _make_image_model(BaseInputImage, "baseInputImage")
+            annotations["baseInputImage"] = base_input_cls
+            namespace["baseInputImage"] = Field(default_factory=base_input_cls)
+
+            for i in range(1, default_count + 1):
+                field_name = f"baseOutputImage{i}"
+                cls_i = _make_image_model(BaseInputImage, field_name)
+                annotations[field_name] = cls_i
+                namespace[field_name] = Field(default_factory=cls_i)
+
+        elif "Outputs" in name or name == "ExecutorDynamicOutputs":
+            base_output_cls = _make_image_model(BaseOutputImage, "baseOutputImage")
+            annotations["baseOutputImage"] = base_output_cls
+            namespace["baseOutputImage"] = Field(default_factory=base_output_cls)
+
+            for i in range(1, default_count + 1):
+                field_name = f"baseOutputImage{i}"
+                cls_i = _make_image_model(BaseOutputImage, field_name)
+                annotations[field_name] = cls_i
+                namespace[field_name] = Field(default_factory=cls_i)
+
+        namespace["__annotations__"] = annotations
+        return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+
+# Apply the dynamic compilation metaclass onto your target structures
+class ExecutorDynamicInputs(Inputs, metaclass=DynamicFieldsMetaclass):
+    #baseInputImage: BaseInputImage (Example, must be field name same as BaseInputImage's name field) 
+    pass
+
+class ExecutorDynamicOutputs(Outputs, metaclass=DynamicFieldsMetaclass):
+    #baseOutputImage: BaseOutputImage (Example, must be field name same as BaseOutputImage's name field) 
+    pass
+
+class ExecutorDynamicConfigs(Configs):
+    inputOutputCount: InputOutputCount
+
+class ExecutorDynamicRequest(Request):
+    inputs: Optional[ExecutorDynamicInputs]
+    configs: ExecutorDynamicConfigs
+    class Configs: json_schema_extra = {"target": "configs"}
+
+class ExecutorDynamicResponse(Response):
+    outputs: ExecutorDynamicOutputs
+
+class ExecutorDynamic(Config):
+    name: Literal["ExecutorDynamic"] = "ExecutorDynamic"
+    value: Union[ExecutorDynamicRequest, ExecutorDynamicResponse]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "ExecutorDynamic"
+        json_schema_extra = {"target": {"value": 0}}
+# endregion Dynamic
+
+
+# Image Class
 class InputImage(Input):
     name: Literal["inputImage"] = "inputImage"
     value: Union[List[Image],Image]
@@ -189,7 +293,7 @@ class PackageExecutor2(Config):
         title = "PackageExecutor2"
         json_schema_extra = {
             "target": {
-                "value": 1
+                "value": 0
             }
         }
 
@@ -199,7 +303,7 @@ class ConfigExecutor(Config):
         ConfigExecutor
     """
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[PackageExecutor1, PackageExecutor2]
+    value: Union[PackageExecutor1, PackageExecutor2, ExecutorDynamic]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
